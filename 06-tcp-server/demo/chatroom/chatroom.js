871fa73917da07ef4e488e3a82ebaf944b318133
JB Tellez <jb.tellez@gmail.com>
2018-05-29 13:49:18 -0700

    guards against in valid payload

--- 06-tcp-server/demo/chatroom/chatroom.js
+++ 06-tcp-server/demo/chatroom/chatroom.js
@@ -58,7 +58,7 @@ let parse = (buffer) => {
   let text = buffer.toString().trim();
   if ( !text.startsWith('@') ) { return null; }
   let [command,payload] = text.split(/\s+(.*)/);
-  let [target,message] = payload.split(/\s+(.*)/);
+  let [target,message] = payload ? payload.split(/\s+(.*)/) : [];
   return {command,payload,target,message};
 
 };
