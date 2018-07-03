d3e7e6eb8bf1d8db365a6e01bc8b1d3607f76b66
John Cokos <highlander.44@gmail.com>
2018-07-03 16:47:27 -0700

    updated demo code

--- 18-oauth/scaffold/auth-server/src/auth/model.js
+++ 18-oauth/scaffold/auth-server/src/auth/model.js
@@ -23,45 +23,6 @@ userSchema.pre('save', function(next) {
     .catch( error => {throw error;} );
 });
 
-userSchema.statics.createFromOAuth = function(incoming) {
-  /*
-    {
-      kind: 'plus#personOpenIdConnect',
-      sub: '100592365129823370453',
-      name: 'John Cokos',
-      given_name: 'John',
-      family_name: 'Cokos',
-      picture: 'https://lh4.googleusercontent.com/-qN0rHFTCPXY/AAAAAAAAAAI/AAAAAAAAAAw/lGUgjyX0vIc/photo.jpg?sz=50',
-      email: 'john@codefellows.com',
-      email_verified: 'true',
-      locale: 'en',
-      hd: 'codefellows.com'
-    }
-   */
-
-  if ( ! incoming || ! incoming.email ) {
-    return Promise.reject('VALIDATION ERROR: missing username/email or password ');
-  }
-
-  return this.findOne({email:incoming.email})
-    .then(user => {
-      if ( ! user ) { throw new Error ('User Not Found'); }
-      console.log('Welcome Back', user.username);
-      return user;
-    })
-    .catch( error => {
-    // Create the user
-      let username = incoming.email;
-      let password = 'none';
-      return this.create({
-        username: username,
-        password: password,
-        email: incoming.email,
-      });
-    });
-
-};
-
 // If we got a user/password, compare them to the hashed password
 // return the user instance or an error
 userSchema.statics.authenticate = function(auth) {
