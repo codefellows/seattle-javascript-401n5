7af187f3e1548b20823addeeeefb93420d01bf37
JB Tellez <jb.tellez@gmail.com>
2018-08-16 00:07:24 -0700

    fixes dupe export

--- 33-redux-middleware/demos/middleware-demo/src/reducers/store.js
+++ 33-redux-middleware/demos/middleware-demo/src/reducers/store.js
@@ -3,7 +3,6 @@ import sweetState from './sweet';
 import savoryState from './savory';
 import reporter from './redux-reporter.js'
 
-export default () => createStore(reducer, applyMiddleware(reporter))
 const rootReducer = combineReducers({
   sweetState,
   savoryState,
